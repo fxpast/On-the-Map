fb81a01ebaa21efd788d38cfc3dc3a498f245d43
pastouret roger <pastouretroger@gmail.com>
2016-05-02 15:45:09 +0200

    LoginViewController

---
+++ On the Map/InfoPostViewController.swift
@@ -0,0 +1,22 @@
+//
+//  InfoPostViewController.swift
+//  On the Map
+//
+//  Created by MacbookPRV on 02/05/2016.
+//  Copyright © 2016 Pastouret Roger. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class InfoPostViewController : UIViewController {
+    
+    
+    @IBAction func ActionCancel(sender: AnyObject) {
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+}
---
+++ On the Map/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  On the Map
+//
+//  Created by MacbookPRV on 02/05/2016.
+//  Copyright © 2016 Pastouret Roger. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class LoginViewController : UIViewController {
+    
+    
+    
+}
